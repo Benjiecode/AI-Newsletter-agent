eca97b65dde09817a025ace6f40a1308b37250a0
Benjiecode <benjie.kalala@gmail.com>
2025-06-12 00:12:06 +0100

    Scraper, now formats in JSON & Python dictionary is extracted for term useage

--- scraper.py
+++ scraper.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 import os
 import dotenv
 import google.generativeai as genai
+import json
 
 #get environment variables from .env file
 dotenv.load_dotenv()
@@ -20,17 +21,27 @@ chat = model.start_chat()
 # Loop for feeding info to AI
 while True:
     user_input = input("You: ")
+    if user_input.lower() in {"exit", "quit"}:
+        print("Goodbye!!!")
+        break
     response = chat.send_message(user_input)
-    print("Gemini:", response.text)
+    #print("Gemini:", response.text)
 
-    response = chat.send_message("Turn this request into a list of 5 keywords that would help me find news articles online.")
-    print("Keywords:", response.text)
-
-    #Get AI to summarise what it just found into some keywords, and find sources like websites etc...
+    response = chat.send_message("Return this request ONLY in JSON format with fields for 'keywords', 'search_terms', and 'websites' only.") #Findings are put into JSON format so that it can be parsed into variables that can be easily re-used in my code
+    '''
+    ``` in my JSON outputted text is blocking my json.loads() function so... We find all text before and after the {}
+    '''
+    gemini_response = response.text
+    start = gemini_response.find('{')
+    end = gemini_response.rfind('}') + 1
+    clean_json = gemini_response[start:end]
+    Scraper_data = json.loads(clean_json)
+    print("Here's what I found!!!\n", Scraper_data)
     
-    #response.text
+    #for key in Scraper_data: #enter json into my pythopn dictionary...
+
     '''
     What i need to do is now, Look for a way to parse this through and rather than printing a bunch of information to the user. I wan the AI to create keywords, and store these + websites into a list/dict.
     '''
-    if user_input.lower() in {"exit", "quit"}:
-        break
+    
+
